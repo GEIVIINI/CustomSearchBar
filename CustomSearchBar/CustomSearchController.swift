ccfe27cebc98bfa4de1177d386357b97673dde9f
GEIVIINI <pai.pp61@gmail.com>
2018-07-09 10:39:24 +0700

    -all

--- CustomSearchBar/CustomSearchController.swift
+++ CustomSearchBar/CustomSearchController.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-class CustomSearchController: UISearchController {
+protocol CustomSearchControllerDelegate {
+    func didStartSearching()
+    
+    func didTapOnSearchButton()
+    
+    func didTapOnCancelButton()
+    
+    func didChangeSearchText(searchText: String)
+}
 
+
+class CustomSearchController: UISearchController, UISearchBarDelegate {
+
+    var customSearchBar: CustomSearchBar!
+    
+    var customDelegate: CustomSearchControllerDelegate!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,14 +38,59 @@ class CustomSearchController: UISearchController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: Initialization
+    
+    init(searchResultsController: UIViewController!, searchBarFrame: CGRect, searchBarFont: UIFont, searchBarTextColor: UIColor, searchBarTintColor: UIColor) {
+        super.init(searchResultsController: searchResultsController)
+        
+        configureSearchBar(frame: searchBarFrame, font: searchBarFont, textColor: searchBarTextColor, bgColor: searchBarTintColor)
     }
-    */
-
+    
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
+    // MARK: Custom functions
+    
+    func configureSearchBar(frame: CGRect, font: UIFont, textColor: UIColor, bgColor: UIColor) {
+        customSearchBar = CustomSearchBar(frame: frame, font: font , textColor: textColor)
+        
+        customSearchBar.barTintColor = bgColor
+        customSearchBar.tintColor = textColor
+        customSearchBar.showsBookmarkButton = false
+        customSearchBar.showsCancelButton = true
+        
+        customSearchBar.delegate = self
+    }
+    
+    
+    // MARK: UISearchBarDelegate functions
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        customDelegate.didStartSearching()
+    }
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        customSearchBar.resignFirstResponder()
+        customDelegate.didTapOnSearchButton()
+    }
+    
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        customSearchBar.resignFirstResponder()
+        customDelegate.didTapOnCancelButton()
+    }
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        customDelegate.didChangeSearchText(searchText: searchText)
+    }
+    
 }
