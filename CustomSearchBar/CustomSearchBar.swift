fc0cb7486067547c174e6004fceb324dd1f1632f
GEIVIINI <pai.pp61@gmail.com>
2018-07-09 10:38:57 +0700

    first commit

---
+++ CustomSearchBar/CustomSearchBar.swift
@@ -0,0 +1,21 @@
+//
+//  CustomSearchBar.swift
+//  CustomSearchBar
+//
+//  Created by Gabriel Theodoropoulos on 8/9/15.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+
+import UIKit
+
+class CustomSearchBar: UISearchBar {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
